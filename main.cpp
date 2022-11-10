ed6ab5a30ad151e5092add139a2afaa001c4a6a4
Olena Babii <gracetomlinson26@gmail.com>
2022-11-10 20:58:39 +0200

    ver 1.0

--- main.cpp
+++ main.cpp
@@ -1,10 +1,7 @@
 #define GL_SILENCE_DEPRECATION
-
-
-#include <OpenGL/gl.h>		// Header File For The OpenGL32 Library
-#include <OpenGL/glu.h>		// Header File For The GLu32 Library
-#include <GLUT/glut.h>      // Header File For The GLut Library
-#include <iostream>
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#include <GLUT/glut.h>
 
 using namespace std;
 
@@ -14,25 +11,7 @@ GLvoid InitGL(GLvoid);
 GLvoid DrawGLScene(GLvoid);
 GLvoid ReSizeGLScene(int Width, int Height);
 void drawAnimal();
-
-void Go(int key, int x, int y)
-{
-    switch(key)
-    {
-        case 'a':
-            x1-=1.0;
-            break;
-        case 'd':
-            x1+=1.0;
-            break;
-        case 'w':
-            y1+=0.0;
-            break;
-        case 's':
-            y1-=0.0;
-            break;
-    }
-}
+void move(int key, int x, int y);
 
 int main(int argc, char** argv)
 {
@@ -42,24 +21,21 @@ int main(int argc, char** argv)
     glutCreateWindow (argv[0]);
 
     InitGL();
-    glutDisplayFunc(DrawGLScene);	// tells GLUT that to use the DrawGLScene function when we want the scene drawn
+    glutDisplayFunc(DrawGLScene);
     glutReshapeFunc(ReSizeGLScene);
-    glutSpecialFunc(Go);
+    glutSpecialFunc(move);
     glutIdleFunc(DrawGLScene);
-    glutMainLoop();	// starts the program. Once called, program only returns to main() when quitting
+    glutMainLoop();
 
     return 0;
 }
 
-
-
-// All the drawing code. Anything to be displayed on the screen should go in here.
 GLvoid DrawGLScene(GLvoid)
 {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);		// Clear The Screen And The Depth Buffer
-    glLoadIdentity();						// Reset the view
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
 
-    glTranslatef(-1.5f + x1,0.0f + y1,-13.0f);			// Move left 1.5 units, and in to the screen 6.0 units
+    glTranslatef(-1.5f + x1,0.0f + y1,-13.0f);
     glColor3ub(126.0f, 191.0f, 69.0f);
     drawAnimal();
     glPolygonMode(GL_FRONT_AND_BACK,GL_LINE);
@@ -70,83 +46,87 @@ GLvoid DrawGLScene(GLvoid)
     glFlush();
 }
 
-
-
-// Perform the setup for OpenGL: colour for clearing the screen, turning on the depth buffer,
-// enable smooth shading, etc. This routine is not called until the OpenGL window has been created.
-// Note: this function returns a value, but we'll ignore it for now
 GLvoid InitGL(GLvoid)
 {
-    glShadeModel(GL_SMOOTH);	// Enable Smooth Shading (blends colours across a polygon/smoothed lighting
-    glClearColor(0.79f, 0.55f, 0.82f, 1.0f);	// Green Background (RGBA; 0.0f = darkest, 1.0f = brightest)
+    glShadeModel(GL_SMOOTH);
+    glClearColor(0.79f, 0.55f, 0.82f, 1.0f);
 
+    glClearDepth(1.0f);
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LEQUAL);
 
-    // Set-up the depth buffer
-    glClearDepth(1.0f);		// Depth Buffer Setup
-    glEnable(GL_DEPTH_TEST);	// Enables Depth Testing
-    glDepthFunc(GL_LEQUAL);		// The Type Of Depth Testing To Do
-
-    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);	// Really Nice Perspective Calculations
+    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
 
 }
 
-
-
-// - Resize the OpenGL scene each time the window has been resized.
-// - This will be called at least once when the program is first run in order to set-up the perspective view.
-// - The OpenGL scene will be resized based on the width and height of the window it's being displayed within.
 GLvoid ReSizeGLScene(int Width, int Height)
 {
-    glViewport(0,0,Width,Height);	// Reset the current viewport.
+    glViewport(0,0,Width,Height);
 
-    // The following lines set-up the screen for perspective view (things in the distance are smaller).
-    // The perspective is calculated with a 45 degree viewing angle based on the window's width/height
-    // 0.f, 100.0f are the starting and ending points for how deep we can draw in to the screen
-    glMatrixMode(GL_PROJECTION);	// Select The Projection Matrix
-    glLoadIdentity();				// Reset The Projection Matrix
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
 
-    gluPerspective(45.0f,(GLfloat)Width/(GLfloat)Height,0.1f,100.0f);	// Calculate The Aspect Ratio Of The Window
+    gluPerspective(45.0f,(GLfloat)Width/(GLfloat)Height,0.1f,100.0f);
 
-    glMatrixMode(GL_MODELVIEW);		// Select The Modelview Matrix
-    glLoadIdentity();			// Reset The Modelview Matrix
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
 }
 
 void drawAnimal() {
     glBegin(GL_TRIANGLES); // Drawing Using Triangles
-    glVertex3f( 0.0f, 1.0f, 0.0f);		// Top
-    glVertex3f(0.0f,-1.0f, 0.0f);		// Bottom Left
-    glVertex3f( 2.0f,-1.0f, 0.0f);		// Bottom Right
+        glVertex3f( 0.0f, 1.0f, 0.0f);		// Top
+        glVertex3f(0.0f,-1.0f, 0.0f);		// Bottom Left
+        glVertex3f( 2.0f,-1.0f, 0.0f);		// Bottom Right
     glEnd();
     glBegin(GL_TRIANGLES); // Drawing Using Triangles
-    glVertex3f( 1.0f, 2.5f, 0.0f);		// Top
-    glVertex3f( 0.0f, 1.5f, 0.0f);		// Bottom Left
-    glVertex3f( 1.0f,1.5f, 0.0f);		// Bottom Right
+        glVertex3f( 1.0f, 2.5f, 0.0f);		// Top
+        glVertex3f( 0.0f, 1.5f, 0.0f);		// Bottom Left
+        glVertex3f( 1.0f,1.5f, 0.0f);		// Bottom Right
     glEnd();
     glBegin(GL_TRIANGLES); // Drawing Using Triangles
-    glVertex3f( 3.0f, 0.0f, 0.0f);		// Top
-    glVertex3f( 1.0f, 0.0f, 0.0f);		// Bottom Left
-    glVertex3f( 3.0f,-2.0f, 0.0f);		// Bottom Right
+        glVertex3f( 3.0f, 0.0f, 0.0f);		// Top
+        glVertex3f( 1.0f, 0.0f, 0.0f);		// Bottom Left
+        glVertex3f( 3.0f,-2.0f, 0.0f);		// Bottom Right
     glEnd();
     glBegin(GL_TRIANGLES); // Drawing Using Triangles
-    glVertex3f( 4.0f, 1.0f, 0.0f);		// Top
-    glVertex3f( 3.0f, 0.0f, 0.0f);		// Bottom Left
-    glVertex3f( 4.0f,0.0f, 0.0f);		// Bottom Right
+        glVertex3f( 4.0f, 1.0f, 0.0f);		// Top
+        glVertex3f( 3.0f, 0.0f, 0.0f);		// Bottom Left
+        glVertex3f( 4.0f,0.0f, 0.0f);		// Bottom Right
     glEnd();
     glBegin(GL_TRIANGLES); // Drawing Using Triangles
-    glVertex3f( 3.0f,-1.0f, 0.0f);	    // Top
-    glVertex3f( 3.0f,-3.0f, 0.0f);		// Bottom Left
-    glVertex3f( 4.0f,-2.0f, 0.0f);			// Bottom Right
+        glVertex3f( 3.0f,-1.0f, 0.0f);	    // Top
+        glVertex3f( 3.0f,-3.0f, 0.0f);		// Bottom Left
+        glVertex3f( 4.0f,-2.0f, 0.0f);			// Bottom Right
     glEnd();
     glBegin(GL_QUADS);				// Draw A Quad
-    glVertex3f(-1.0f, 1.5f, 0.0f);		// Top Left
-    glVertex3f( 0.0f, 1.5f, 0.0f);		// Top Right
-    glVertex3f( 0.0f,0.5f, 0.0f);		// Bottom Right
-    glVertex3f(-1.0f,0.5f, 0.0f);		// Bottom Left
+        glVertex3f(-1.0f, 1.5f, 0.0f);		// Top Left
+        glVertex3f( 0.0f, 1.5f, 0.0f);		// Top Right
+        glVertex3f( 0.0f,0.5f, 0.0f);		// Bottom Right
+        glVertex3f(-1.0f,0.5f, 0.0f);		// Bottom Left
     glEnd();
     glBegin(GL_QUADS);				// Draw A Quad
-    glVertex3f(-0.5f,-1.0f, 0.0f);	    // Top Left
-    glVertex3f( 1.0f,-1.0f, 0.0f);		// Top Right
-    glVertex3f( 0.5f,-2.0f, 0.0f);		// Bottom Right
-    glVertex3f(-1.0f,-2.0f, 0.0f);		// Bottom Left
+        glVertex3f(-0.5f,-1.0f, 0.0f);	    // Top Left
+        glVertex3f( 1.0f,-1.0f, 0.0f);		// Top Right
+        glVertex3f( 0.5f,-2.0f, 0.0f);		// Bottom Right
+        glVertex3f(-1.0f,-2.0f, 0.0f);		// Bottom Left
     glEnd();
+}
+
+void move(int key, int x, int y)
+{
+    switch(key)
+    {
+        case 'a':
+            x1-=1.0;
+            break;
+            case 'd':
+                x1+=1.0;
+                break;
+                case 'w':
+                    y1+=0.0;
+                    break;
+                    case 's':
+                        y1-=0.0;
+                        break;
+    }
 }
