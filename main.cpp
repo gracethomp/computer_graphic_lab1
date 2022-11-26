69be4b1cac6a05d496ea64cd331f2aea29ed0a13
Olena Babii <gracetomlinson26@gmail.com>
2022-11-26 20:32:25 +0200

    ver 1.2

--- main.cpp
+++ main.cpp
@@ -3,26 +3,23 @@
 #include <OpenGL/glu.h>
 #include <GLUT/glut.h>
 
-
-
 using namespace std;
 
 float x1=0.0f, y2 = 0.0f;
 
-GLvoid InitGL(GLvoid);
-GLvoid DrawGLScene(GLvoid);
-GLvoid ReSizeGLScene(int Width, int Height);
+void DrawGLScene();
+void ReSizeGLScene(int Width, int Height);
 void drawAnimal();
 void move(int key, int x, int y);
 
 int main(int argc, char** argv)
 {
-    glutInit(&argc, argv);
-    glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
-    glutInitWindowSize (1000, 1000);
-    glutCreateWindow (argv[0]);
+    glutInit(&argc, argv); //инициализируем библиотеку glut
+    glutInitWindowSize (1500, 1000); //устанавливаем размеры окна
+    glutCreateWindow ("Dog (or rabbit idk) is moving..."); //создаем окно и даем ему название
 
-    InitGL();
+    glClearColor(0.79f, 0.55f, 0.82f, 1.0f); //задаем цвет фона
+
     glutDisplayFunc(DrawGLScene);
     glutReshapeFunc(ReSizeGLScene);
     glutSpecialFunc(move);
@@ -37,35 +34,22 @@ GLvoid DrawGLScene(GLvoid)
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
 
-    glTranslatef(-1.5f + x1,0.0f,-13.0f);
+    glTranslatef(-1.5f + x1,0.0f + y2 ,-13.0f);
     glColor3ub(126.0f, 191.0f, 69.0f);
     drawAnimal();
     glPolygonMode(GL_FRONT_AND_BACK,GL_LINE);
     glColor3ub(0.0f, 0.0f, 0.0f);
     drawAnimal();
-    glColor3ub(126.0f, 191.0f, 69.0f);
     glPolygonMode(GL_FRONT_AND_BACK,GL_FILL);
     glFlush();
 }
 
-GLvoid InitGL(GLvoid)
-{
-    glShadeModel(GL_SMOOTH);
-    glClearColor(0.79f, 0.55f, 0.82f, 1.0f);
-
-    glClearDepth(1.0f);
-    glEnable(GL_DEPTH_TEST);
-    glDepthFunc(GL_LEQUAL);
-
-    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
-
-}
 
 GLvoid ReSizeGLScene(int Width, int Height)
 {
-    glViewport(0,0,Width,Height);
+    glViewport(0,0,Width,Height); //задаем окно просмотра
 
-    glMatrixMode(GL_PROJECTION);
+    glMatrixMode(GL_PROJECTION); //
     glLoadIdentity();
 
     gluPerspective(45.0f,(GLfloat)Width/(GLfloat)Height,0.1f,100.0f);
@@ -76,41 +60,41 @@ GLvoid ReSizeGLScene(int Width, int Height)
 
 void drawAnimal() {
     glBegin(GL_TRIANGLES); // Drawing Using Triangles
-        glVertex3f( 0.0f, 1.0f, 0.0f);		// Top
-        glVertex3f(0.0f,-1.0f, 0.0f);		// Bottom Left
-        glVertex3f( 2.0f,-1.0f, 0.0f);		// Bottom Right
+        glVertex2f( 0.0f, 1.0f);		// Top
+        glVertex2f(0.0f,-1.0f);		// Bottom Left
+        glVertex2f( 2.0f,-1.0f);		// Bottom Right
     glEnd();
     glBegin(GL_TRIANGLES); // Drawing Using Triangles
-        glVertex3f( 1.0f, 2.5f, 0.0f);		// Top
-        glVertex3f( 0.0f, 1.5f, 0.0f);		// Bottom Left
-        glVertex3f( 1.0f,1.5f, 0.0f);		// Bottom Right
+        glVertex2f( 1.0f, 2.5f);		// Top
+        glVertex2f( 0.0f, 1.5f);		// Bottom Left
+        glVertex2f( 1.0f,1.5f);		// Bottom Right
     glEnd();
     glBegin(GL_TRIANGLES); // Drawing Using Triangles
-        glVertex3f( 3.0f, 0.0f, 0.0f);		// Top
-        glVertex3f( 1.0f, 0.0f, 0.0f);		// Bottom Left
-        glVertex3f( 3.0f,-2.0f, 0.0f);		// Bottom Right
+        glVertex2f( 3.0f, 0.0f);		// Top
+        glVertex2f( 1.0f, 0.0f);		// Bottom Left
+        glVertex2f( 3.0f,-2.0f);		// Bottom Right
     glEnd();
     glBegin(GL_TRIANGLES); // Drawing Using Triangles
-        glVertex3f( 4.0f, 1.0f, 0.0f);		// Top
-        glVertex3f( 3.0f, 0.0f, 0.0f);		// Bottom Left
-        glVertex3f( 4.0f,0.0f, 0.0f);		// Bottom Right
+        glVertex2f( 4.0f, 1.0f);		// Top
+        glVertex2f( 3.0f, 0.0f);		// Bottom Left
+        glVertex2f( 4.0f,0.0f);		// Bottom Right
     glEnd();
     glBegin(GL_TRIANGLES); // Drawing Using Triangles
-        glVertex3f( 3.0f,-1.0f, 0.0f);	    // Top
-        glVertex3f( 3.0f,-3.0f, 0.0f);		// Bottom Left
-        glVertex3f( 4.0f,-2.0f, 0.0f);			// Bottom Right
+        glVertex2f( 3.0f,-1.0f);	    // Top
+        glVertex2f( 3.0f,-3.0f);		// Bottom Left
+        glVertex2f( 4.0f,-2.0f);			// Bottom Right
     glEnd();
     glBegin(GL_QUADS);				// Draw A Quad
-        glVertex3f(-1.0f, 1.5f, 0.0f);		// Top Left
-        glVertex3f( 0.0f, 1.5f, 0.0f);		// Top Right
-        glVertex3f( 0.0f,0.5f, 0.0f);		// Bottom Right
-        glVertex3f(-1.0f,0.5f, 0.0f);		// Bottom Left
+        glVertex2f(-1.0f, 1.5f);		// Top Left
+        glVertex2f( 0.0f, 1.5f);		// Top Right
+        glVertex2f( 0.0f,0.5f);		// Bottom Right
+        glVertex2f(-1.0f,0.5f);		// Bottom Left
     glEnd();
     glBegin(GL_QUADS);				// Draw A Quad
-        glVertex3f(-0.5f,-1.0f, 0.0f);	    // Top Left
-        glVertex3f( 1.0f,-1.0f, 0.0f);		// Top Right
-        glVertex3f( 0.5f,-2.0f, 0.0f);		// Bottom Right
-        glVertex3f(-1.0f,-2.0f, 0.0f);		// Bottom Left
+        glVertex2f(-0.5f,-1.0f);	    // Top Left
+        glVertex2f( 1.0f,-1.0f);		// Top Right
+        glVertex2f( 0.5f,-2.0f);		// Bottom Right
+        glVertex2f(-1.0f,-2.0f);		// Bottom Left
     glEnd();
 }
 
@@ -127,11 +111,11 @@ void move(int key, int x, int y)
             break;
         }
         case 'w': {
-            y2 += 0.0;
+            y2 += 1.0;
             break;
         }
         case 's': {
-            y2 -= 0.0;
+            y2 -= 1.0;
             break;
         }
     }
